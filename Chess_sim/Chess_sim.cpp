46a7bf3761bbba674b0dc5805e82b58af9819729
koliruslik <koliruslik@gmail.com>
2025-03-23 19:12:10 +0200

    fix: git problems

---
+++ Chess_sim/Chess_sim.cpp
@@ -0,0 +1,12 @@
+﻿// Chess_sim.cpp: определяет точку входа для приложения.
+//
+#include "raylib"
+#include "Chess_sim.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "H123llo CMake." << endl;
+	return 0;
+}
