b3184d150f8ba94dd8c163d33cf66a5edce39844
koliruslik <koliruslik@gmail.com>
2025-03-23 20:46:26 +0200

    fix: bug with compilation

--- Chess_sim/Chess_sim.cpp
+++ Chess_sim/Chess_sim.cpp
@@ -1,12 +1,12 @@
 ﻿// Chess_sim.cpp: определяет точку входа для приложения.
 //
-#include "raylib"
+#include "raylib.h"
 #include "Chess_sim.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "H123llo CMake." << endl;
+	cout << "H123ll123o CMake." << endl;
 	return 0;
 }
---
+++ build/external/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: E:/KPI/Курсач/2Семестр/Chess_Sim/external/raylib
+# Build directory: E:/KPI/Курсач/2Семестр/Chess_Sim/build/external/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
